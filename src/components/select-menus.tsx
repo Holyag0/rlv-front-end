5bd547a8559a08d5ab453f97eb60d0261f009108
Hiago Santos <hiagosantosince98@gmail.com>
2024-11-22 23:36:45 -0300

    correções para deploy, requisições do site de hospedagem

--- src/components/select-menus.tsx
+++ src/components/select-menus.tsx
@@ -9,28 +9,36 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-
-export function SelectMenus({title,options}) {
+interface SelectMenusProps {
+  title: string;
+  options: string[];
+}
+export function SelectMenus({ title, options }: SelectMenusProps) {
   return (
     <>
+      {" "}
       <div className="flex flex-col">
-        <h4>{title}</h4>
+        {" "}
+        <label className="block text-sm font-medium text-gray-700">
+          {title}
+        </label>{" "}
         <Select>
-          <SelectTrigger className="w-[280px]">
-            <SelectValue placeholder="Selecione a opção" />
-          </SelectTrigger>
+          {" "}
+          <SelectTrigger className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm">
+            {" "}
+            <SelectValue placeholder="Selecione uma opção" />{" "}
+          </SelectTrigger>{" "}
           <SelectContent>
-            <SelectGroup>
-              <SelectLabel>{title=='Origem do documento'?'Origem':'Tipos'}</SelectLabel>
-              {options.map((option, index) => (
-                <SelectItem key={index} value={option}>
-                  {option}
-                </SelectItem>
-              ))}
-            </SelectGroup>
-          </SelectContent>
-        </Select>
-      </div>
+            {" "}
+            {options.map((option, index) => (
+              <SelectItem key={index} value={option}>
+                {" "}
+                {option}{" "}
+              </SelectItem>
+            ))}{" "}
+          </SelectContent>{" "}
+        </Select>{" "}
+      </div>{" "}
     </>
   );
 }
