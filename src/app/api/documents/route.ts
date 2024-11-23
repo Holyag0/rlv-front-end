abdcf07cb0adaaf5d64355311da33a8c5d72859d
Hiago Santos <hiagosantosince98@gmail.com>
2024-11-22 23:44:39 -0300

    correçoes para deploy

--- src/app/api/documents/route.ts
+++ src/app/api/documents/route.ts
@@ -1,30 +1,30 @@
-import { NextRequest, NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
 import prisma from '@/prismaCliente';
 
-export async function GET() { 
-    try { 
-        const documents = await prisma.document.findMany(); 
-        return NextResponse.json(documents, { status: 200 }); 
-    } 
-    catch (error) {
-         return NextResponse.json({ 
-            error: "Erro ao buscar documentos" }, 
-            { status: 500 }); }
-         } 
-         export async function POST(request: NextRequest) { 
-            try { 
-                const { nome, emitente, valor_tributo, valor_liquido, tipo } = await request.json(); 
-                const newDocument = await prisma.document.create({ 
-                    data: { 
-                        nome, 
-                        emitente, 
-                        valor_tributo, 
-                        valor_liquido, 
-                        tipo, 
-                    }, 
-                }); 
-                return NextResponse.json(newDocument, { status: 201 });
-             } catch (error) { 
-                return NextResponse.json({ error: "Erro ao criar documento" }, { status: 500 });
-            }
-        }
+export async function GET() {
+  try {
+    const documents = await prisma.document.findMany();
+    return new Response(JSON.stringify(documents), { status: 200 });
+  } catch (error) {
+    console.error("Erro ao buscar documentos:", error);
+    return new Response(
+      JSON.stringify({ error: "Erro ao buscar documentos" }),
+      { status: 500 }
+    );
+  }
+}
+export async function POST(request: NextRequest) {
+  try {
+    const { nome, emitente, valor_tributo, valor_liquido, tipo } =
+      await request.json();
+    const newDocument = await prisma.document.create({
+      data: { nome, emitente, valor_tributo, valor_liquido, tipo },
+    });
+    return new Response(JSON.stringify(newDocument), { status: 201 });
+  } catch (error) {
+    console.error("Erro ao criar documento:", error);
+    return new Response(JSON.stringify({ error: "Erro ao criar documento" }), {
+      status: 500,
+    });
+  }
+}
